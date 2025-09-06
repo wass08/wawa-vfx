e107ec7bfd1a2e0f744e1160a0440b5280122f3a
wass08 <wass08@gmail.com>
2025-09-06 12:10:31 +0900

    remove unused files

--- packages/src/components/vfxs/core/index.ts
+++
@@ -1,5 +0,0 @@
-export { VFXEmitterCore } from "./VFXEmitterCore";
-export type { VFXEmitterSettings } from "./VFXEmitterCore";
-
-export { VFXParticlesCore } from "./VFXParticlesCore";
-export type { VFXParticlesSettings, EmitCallbackSettings, EmitCallbackSettingsFn } from "./VFXParticlesCore";
